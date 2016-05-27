714c94397ed26441bcc74f50efa99697a4b67128
Adam Stokes <adam.stokes@ubuntu.com>
2016-05-27 12:43:15 -0400

    update script to exit if error in services
    Signed-off-by: Adam Stokes <adam.stokes@ubuntu.com>

--- conjure/steps/01_register-autopilot.sh
+++ conjure/steps/01_register-autopilot.sh
@@ -6,7 +6,17 @@
 . /usr/share/conjure-up/hooklib/common.sh
 
 landscape_exec=$SCRIPTPATH/configure-landscape
-while ! `$landscape_exec --admin-email "root@example.com" --admin-password "ubuntu123" --admin-name "administrator" --system-email "root@example.com" --maas-host "$MAAS_SERVER" --maas-apikey "$MAAS_OAUTH"`; do sleep 5; done
+
+if [ $(unitStatus landscape-server 0) = "error" ]; then
+    exposeResult "There is an error with landscape-server, please check juju status." 1 "false"
+fi
+
+$landscape_exec --admin-email "root@example.com" --admin-password "ubuntu123" --admin-name "administrator" --system-email "root@example.com" --maas-host "$MAAS_SERVER" --maas-apikey "$MAAS_OAUTH"
+RET=$?
+
+if [ $RET -gt 0 ]; then
+    exposeResult "Unable to register against Landscape server, please check your deployment." 1 "false"
+fi
 
 result_message="Access Autopilot: http://$hostname/account/standalone/openstack l: root@example.com p: ubuntu123"
 
